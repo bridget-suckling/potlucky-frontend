68168d6197056f9b91151b96bb9db766a3729a6a
Bridget Suckling <creativehq@Bridgets-Macbook-Pro.local>
2022-12-24 10:16:56 +1300

    people data and guest component

---
+++ src/Guests.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import peopleData from './people.json'
+
+function Guests() {
+
+  console.log(peopleData)
+
+  return (
+    <div>
+       <h2>Guest list</h2>
+      <ul>
+        <li key="1">Bridget</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Guests;
